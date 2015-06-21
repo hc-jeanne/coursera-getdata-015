fbea1911b5ddb1957ad5e567fb7ad1136187e461
hc-jeanne <huichenjyu@yahoo.com>
2015-06-22 05:27:27 +0800

    as a function

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 ##
 ## function mergeTrainTest -- to merge the train and test data
+## function runAnalysis -- tidy and analysis : return the step 5 result data set
 
 mergeTrainTest <- function(trainFile, testFile) {
   dir <- "UCI HAR Dataset/"
@@ -17,36 +18,39 @@ mergeTrainTest <- function(trainFile, testFile) {
   data
 }
 
-## 1. Merges the training and the test sets:
-x <- mergeTrainTest("X_train.txt", "X_test.txt")
-y <- mergeTrainTest("y_train.txt", "y_test.txt")
-subject <- mergeTrainTest("subject_train.txt", "subject_test.txt")
+runAnalysis <- function() {
+  ## 1. Merges the training and the test sets:
+  x <- mergeTrainTest("X_train.txt", "X_test.txt")
+  y <- mergeTrainTest("y_train.txt", "y_test.txt")
+  subject <- mergeTrainTest("subject_train.txt", "subject_test.txt")
 
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-feature <- read.table("UCI HAR Dataset/features.txt")
+  ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+  feature <- read.table("UCI HAR Dataset/features.txt")
 
-#-- checks if the "mean()" or "std()" or "meanFreq()" in the features:
-extractLogic <- grepl("mean()", feature$V2, fixed=TRUE) | grepl("std()", feature$V2, fixed=TRUE)
-# if "meanFreq()" is not include in the "mean", comments the following line:
-extractLogic <- extractLogic | grepl("meanFreq()", feature$V2, fixed=TRUE)
-feature2 <- feature[extractLogic,]
-x2 <- x[,extractLogic]
+  #-- checks if the "mean()" or "std()" or "meanFreq()" in the features:
+  extractLogic <- grepl("mean()", feature$V2, fixed=TRUE) | grepl("std()", feature$V2, fixed=TRUE)
+  # if "meanFreq()" is not include in the "mean", comments the following line:
+  extractLogic <- extractLogic | grepl("meanFreq()", feature$V2, fixed=TRUE)
+  feature2 <- feature[extractLogic,]
+  x2 <- x[,extractLogic]
 
-## 3. Uses descriptive activity names to name the activities in the data set
-activity <- read.table("UCI HAR Dataset/activity_labels.txt")
+  ## 3. Uses descriptive activity names to name the activities in the data set
+  activity <- read.table("UCI HAR Dataset/activity_labels.txt")
 
-library(plyr)
-yLabel <- join(y,activity, by="V1", type = "inner")
-cNames <- c("subject", "activity", as.character(feature2[,2]))
-x3 <- cbind(subject, yLabel[,2], x2)
+  library(plyr)
+  yLabel <- join(y,activity, by="V1", type = "inner")
+  cNames <- c("subject", "activity", as.character(feature2[,2]))
+  x3 <- cbind(subject, yLabel[,2], x2)
 
-## 4. Appropriately labels the data set with descriptive variable names. 
-x4 <- x3
-colnames(x4) <- cNames
+  ## 4. Appropriately labels the data set with descriptive variable names. 
+  x4 <- x3
+  colnames(x4) <- cNames
 
-## 5. From the data set in step 4, creates a second, independent tidy data set 
-##    with the average of each variable for each activity and each subject.
-#-- group-by subject and activity:
-g <- subset(x4, select=c(subject,activity))
-x4_2 <- subset(x4, select=-c(subject,activity))
-x5 <- aggregate(x4_2, g, mean)
+  ## 5. From the data set in step 4, creates a second, independent tidy data set 
+  ##    with the average of each variable for each activity and each subject.
+  #-- group-by subject and activity:
+  g <- subset(x4, select=c(subject,activity))
+  x4_2 <- subset(x4, select=-c(subject,activity))
+  x5 <- aggregate(x4_2, g, mean)
+  x5
+}
